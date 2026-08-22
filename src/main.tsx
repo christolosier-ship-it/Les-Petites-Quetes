222b981c1668e41f0297d44f7fd60fd5a610b293
Kristo <christo.losier@gmail.com>
2026-08-22 22:26:40 +0200

    refactor: charger les styles Firefly séparément

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import './styles/global.css';
 import './styles/app.css';
 import './styles/forms.css';
 import './styles/world.css';
+import './styles/firefly-world.css';
 import './styles/finalization.css';
 
 const root = document.getElementById('root');
